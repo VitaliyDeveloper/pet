ca8711aaa9f52c09e158001f1c4f039fc012e402
Vitaliy Gerassimov <gerassimov.developer@gmail.com>
2023-07-27 12:53:20 +0600

    1

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+// import Navigation from 'components/Navigation/Navigation';
+// import Footer from 'components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <header>{/* <Navigation /> */}</header>
+      <main style={{ minHeight: '100vh' }}>
+        <Outlet />
+      </main>
+      <footer>{/* <Footer /> */}</footer>
+    </>
+  );
+};
+
+export default Layout;
